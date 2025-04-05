98bb9ea36d549733fd710c3347ef1b6f878a6641
ajmalm <ajmalmundeth1324@gmail.com>
2025-04-05 23:41:46 +0530

    frist code commit bloc

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note/screen/login_screen.dart';
 
-import 'logic/blocs/auth_bloc.dart';
-import 'logic/blocs/auth_event.dart';
-import 'logic/data/repositories/auth_repository.dart';
-import 'logic/data/repositories/notes_repository.dart';
 // import 'data/repositories/auth_repository.dart';
 // import 'data/repositories/notes_repository.dart';
+
+import 'logic/blocs/auth_bloc.dart';
 // import 'logic/blocs/auth_bloc/auth_bloc.dart';
+// import 'logic/blocs/auth_bloc/auth_event.dart';
+import 'logic/blocs/auth_event.dart';
+import 'logic/blocs/notes_bloc/notes_bloc.dart';
+import 'logic/data/repositories/auth_repository.dart';
+import 'logic/data/repositories/notes_repository.dart';
 // import 'presentation/screens/auth/login_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -24,9 +27,15 @@ class MyApp extends StatelessWidget {
       child: MultiBlocProvider(
         providers: [
           BlocProvider(
-              create: (context) =>
-                  AuthBloc(authRepository: context.read<AuthRepository>())
-                    ..add(AppStarted())),
+            create: (context) =>
+                AuthBloc(authRepository: context.read<AuthRepository>())
+                  ..add(AppStarted()),
+          ),
+          // 🔥 FIX HERE: Add NotesBloc
+          BlocProvider(
+            create: (context) =>
+                NotesBloc(notesRepository: context.read<NotesRepository>()),
+          ),
         ],
         child: MaterialApp(
           title: 'Notes App',
